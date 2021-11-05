197cab68cc74bdebc3541879880217971a3f4417
Chanchal Kumar Ghosh <284846+chanchal1987@users.noreply.github.com>
2021-11-05 16:12:14 -0700

    Added some utility methods

--- process.go
+++ process.go
@@ -1,11 +1,49 @@
+// llps provides an API for finding and listing processes in a platform-agnostic way.
 package llps
 
 import sdk "github.com/mitchellh/go-ps"
 
+// Process contains information about a running process.
+//
+// This is generic to all operating systems supported by github.com/mitchellh/go-ps.
 type Process struct {
+	// ParentProcess is the information about the parent process.
 	ParentProcess *Process
-	PID           int
-	Executable    string
+
+	// Pid is the process ID for this process.
+	PID int
+
+	// Executable name running this process. This is not a path to the executable.
+	Executable string
+}
+
+// String returns a human-friendly string for the process.
+func (p *Process) String() string {
+	return p.Executable
+}
+
+// GoToParent returns nth the parent process of the given process.
+func (ps *Process) GoToParent(n int) *Process {
+	if n <= 0 {
+		return ps
+	}
+
+	if ps.ParentProcess == nil {
+		return nil
+	}
+
+	return ps.ParentProcess.GoToParent(n - 1)
+}
+
+// FindExecutable find all the processes matched by the lookup func.
+func (ps *Process) FindExecutable(f func(executable string) bool) (processes []*Process) {
+	for psCopy := ps; psCopy != nil; psCopy = psCopy.ParentProcess {
+		if f(psCopy.Executable) {
+			processes = append(processes, psCopy)
+		}
+	}
+
+	return
 }
 
 func processMap() (map[int]*Process, error) {
@@ -36,19 +74,11 @@ func processMap() (map[int]*Process, error) {
 	return processMap, nil
 }
 
-func FindProcess(pid int) (*Process, error) {
-	processMap, err := processMap()
-	if err != nil {
-		return nil, err
-	}
-
-	if process, ok := processMap[pid]; ok {
-		return process, nil
-	}
-
-	return nil, nil
-}
-
+// Processes returns all processes.
+//
+// This of course will be a point-in-time snapshot of when this method was called. Some operating
+// systems don't provide snapshot capability of the process table, in which case the process table
+// returned might contain ephemeral entities that happened to be running when this was called.
 func Processes() ([]*Process, error) {
 	processMap, err := processMap()
 	if err != nil {
@@ -61,4 +91,20 @@ func Processes() ([]*Process, error) {
 	}
 
 	return processes, nil
+}
+
+// FindProcess looks up a single process by pid.
+//
+// Process will be nil and error will be nil if a matching process is not found.
+func FindProcess(pid int) (*Process, error) {
+	processMap, err := processMap()
+	if err != nil {
+		return nil, err
+	}
+
+	if process, ok := processMap[pid]; ok {
+		return process, nil
+	}
+
+	return nil, nil
 }
